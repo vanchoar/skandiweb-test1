ec66aa9ade87637f3809468eb8e797b1ab8d182a
vanchoar <vanco.ar@gmail.com>
2025-04-09 14:28:56 +0200

    Updated production files + .env

--- backend/db/db.php
+++ backend/db/db.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Database;
-
+// require_once __DIR__ . '/../vendor/autoload.php';
+use Dotenv\Dotenv;
 class Database {
     private static $connection = null;
 
@@ -10,10 +11,18 @@ class Database {
             ini_set('display_startup_errors', 1);
             error_reporting(E_ALL);
 
-            $host = "localhost";
-            $dbname = "scandiweb3";
-            $username = "root";
-            $password = "palacinka";
+            // $host = "localhost";
+            // $dbname = "scandiweb3";
+            // $username = "root";
+            // $password = "palacinka";
+
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/..'); // Adjust path to your project root
+            $dotenv->load();
+
+            $host = $_ENV['DB_HOST'];
+            $dbname = $_ENV['DB_NAME'];
+            $username = $_ENV['DB_USER'];
+            $password = $_ENV['DB_PASS'];
 
             try {
                 self::$connection = new \PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInitc38c7dd4ea92379d92497be42e5ed525
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInitc38c7dd4ea92379d92497be42e5ed525::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
